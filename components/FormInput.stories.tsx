9d010c9dae925481841c075c5115abc9f3aef216
Laurent Sutterlity <laurent@sutterlity.studio>
2024-04-17 11:43:24 +0200

    feat: add FormInput component

---
+++ components/FormInput.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import FormInput from "./FormInput"
+
+const meta: Meta<typeof FormInput> = {
+  title: "FormInput",
+  component: FormInput,
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof FormInput>
+
+export const Default: Story = {
+  args: {
+    placeholder: "Placeholder",
+  },
+}
+
+export const Size: Story = {
+  args: {
+    ...Default.args,
+  },
+  render: (args) => (
+    <div className="flex flex-col gap-4">
+      <FormInput {...args} size="xs" />
+      <FormInput {...args} size="sm" />
+      <FormInput {...args} />
+      <FormInput {...args} size="lg" />
+    </div>
+  ),
+}
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    disabled: true,
+  },
+}
+
+export const ReadOnly: Story = {
+  args: {
+    ...Default.args,
+    readOnly: true,
+  },
+}
---
+++ components/FormInput.tsx
@@ -0,0 +1,33 @@
+import { Input, InputProps } from "react-aria-components"
+import { tv } from "tailwind-variants"
+
+import { cn } from "@lib/utils"
+
+const inputStyles = tv({
+  base: "ra-focus-ring block w-full rounded-md border-0 ring-1 ring-inset  transition data-[focused=true]:ring-2 data-[focus-visible=true]:ring-1 text-gray-900 ring-input placeholder:text-input-placeholder data-[focused=true]:ring-primary data-[focus-visible=true]:ring-input invalid:ring-error invalid:text-error invalid:placeholder-error-400 invalid:bg-error-50 disabled:bg-input-disabled",
+  variants: {
+    size: {
+      xs: "px-2 py-1 text-xs",
+      sm: "px-2 py-1 text-sm",
+      md: "px-3 py-1.5 text-sm",
+      lg: "px-4 py-2.5 text-sm",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+    state: "default",
+  },
+})
+
+interface FormInputProps extends Omit<InputProps, "size"> {
+  /**
+   * The size of the input
+   */
+  size?: "xs" | "sm" | "md" | "lg"
+}
+
+const FormInput = ({ className, size, ...rest }: FormInputProps) => {
+  return <Input className={cn(inputStyles({ size }), className)} {...rest} />
+}
+
+export default FormInput
